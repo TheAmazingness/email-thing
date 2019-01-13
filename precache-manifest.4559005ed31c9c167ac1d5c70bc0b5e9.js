988587676a436a309897c549546d1e69434c2b5b
Tristan Peng <pengt@catlin.edu>
2019-01-12 23:28:24 -0800

    Updates

---
+++ precache-manifest.4559005ed31c9c167ac1d5c70bc0b5e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "35d95a5a98c30127eb02",
+    "url": "/ablemail/static/js/main.35d95a5a.chunk.js"
+  },
+  {
+    "revision": "ad1532231125f7f9ca7b",
+    "url": "/ablemail/static/js/1.ad153223.chunk.js"
+  },
+  {
+    "revision": "37d0c7c235ce5129c1cd",
+    "url": "/ablemail/static/js/runtime~main.37d0c7c2.js"
+  },
+  {
+    "revision": "91312676e6c69669bf49c68af639d171",
+    "url": "/ablemail/index.html"
+  }
+];
