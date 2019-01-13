f182067d8feb690e000fe71c0119e044d85e62d8
Tristan Peng <pengt@catlin.edu>
2019-01-12 22:42:44 -0800

    Fix issue when MediaRecorder is inactive

--- src/Recorder.js
+++ src/Recorder.js
@@ -3,6 +3,7 @@ const VEMAIL = JSON.parse(window.localStorage.getItem('vemail'));
 export default class Recorder {
   constructor() {
     if (VEMAIL) {
+      this.isRecording = false;
       this.result = new Promise((resolve) => {
         navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
           this.mediaRecorder = new MediaRecorder(stream);
@@ -24,15 +25,17 @@ export default class Recorder {
   }
 
   start() {
-    if (VEMAIL) {
+    if (VEMAIL && !this.isRecording) {
       this.mediaRecorder.start();
+      this.isRecording = true;
       console.log('Starting recording...');
     }
   }
 
   stop() {
-    if (VEMAIL) {
+    if (VEMAIL && this.isRecording) {
       this.mediaRecorder.stop();
+      this.isRecording = false;
       console.log('Stopping recording...');
     }
   }
