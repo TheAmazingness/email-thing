4081ec4ac7e2f65c088f06b89fb274240c1f3147
Tristan Peng <pengt@catlin.edu>
2019-01-15 11:26:27 -0800

    Make TTS cancellable

--- src/Voice.js
+++ src/Voice.js
@@ -1,7 +1,12 @@
 import Voice from '@cheapundies/responsive-voice';
 
 const SPEAK = JSON.parse(window.localStorage.getItem('tts'));
+let cancel = false;
 
 export function speak(text) {
-  SPEAK && Voice.speak(text, 'US English Female', { rate: 0.75 });
+  if (SPEAK) {
+    cancel = !cancel;
+    cancel && Voice.cancel();
+    !cancel && Voice.speak(text, 'US English Female', { rate: 0.75 });
+  }
 }
