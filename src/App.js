409e85c1bb56a8a6859be2578cb1b3680a42482c
Tristan Peng <pengt@catlin.edu>
2019-01-23 18:59:41 -0800

    Document App and Mailbox and lots of reorganization

--- src/App.js
+++ src/App.js
@@ -1,229 +1,143 @@
+// Imports ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Imports //
+// React ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 import React from 'react';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import ClientSettings from './ClientSettings';
-import Compose from './Compose';
+// React ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+// Material UI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+import CircularProgress from '@material-ui/core/CircularProgress/CircularProgress';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Mailbox from './Mailbox';
-import Recognition from './Recognition';
-import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
-import Settings from './Settings';
-import SettingsIcon from '@material-ui/icons/Settings'
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { Voice } from './Voice';
 import { withStyles } from '@material-ui/core/styles';
+// Material UI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-const icon = JSON.parse(window.localStorage.getItem('icon'));
-const style = theme => ({
-  appBar: {
-    zIndex: 1400,
-    alignItems: 'center'
-  },
-  auth: {
-    position: 'absolute',
-    right: '-40vw'
-  },
-  body: {
-    height: '100vh',
-    margin: 0,
-    overflow: 'hidden',
-    width: '100vw',
-    display: 'flex'
-  },
-  bold: {
-    fontWeight: icon ? '' : 'bold'
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 12.5,
-    overflowY: 'scroll'
-  },
-  drawer: {
-    flexShrink: 0,
-    width: 400
-  },
-  drawerPaper: {
-    width: 400
-  },
-  icon: {
-    fontSize: icon ? '60pt' : '40pt'
-  },
-  logo: {
-    height: '5vh'
-  },
-  settings: {
-    position: 'absolute',
-    left: '-40vw'
-  },
-  toolbar: theme.mixins.toolbar
-});
+// Other ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+import Mailbox from './Mailbox';
+import SideNav from './SideNav';
+import TopNav from './TopNav'
+// Other ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Imports ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Imports //
 
+// Constants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants //
+const
+  API_KEY = '<API_KEY>',
+  CLIENT_ID = '<CLIENT_ID>',
+  DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
+  SCOPES = 'https://www.googleapis.com/auth/gmail.modify',
+  style = {
+    loading: {
+      height: '100vh',
+      lineHeight: '100vh',
+      textAlign: 'center',
+      width: '100vw'
+    }
+  };
+// Constants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants //
+
+// App Component ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ App Component//
 class App extends React.Component {
+  /** constructor */
   constructor(props) {
     super(props);
-    this.recognition = new Recognition();
-    this.isRecording = false;
-    this.emailCount = 0;
     this.state = {
-      auth: null,
-      compose: false,
-      settings: false,
-      clientSettings: false,
-      recognition: JSON.parse(window.localStorage.getItem('recognition')) ? (
-        <div>
-          <div className={ this.props.classes.toolbar } />
-          <Divider />
-          <ListItem button={ true } onClick={ () => { new Voice('Voice Command').activate(); this.handleRecognition(); } }>
-            <ListItemIcon>
-              <RecordVoiceOverIcon className={ this.props.classes.icon } />
-            </ListItemIcon>
-            <ListItemText>
-              <br />
-              <Typography variant='h3' color='inherit' className={ this.props.classes.bold }>Voice Command</Typography>
-              <br />
-            </ListItemText>
-          </ListItem>
-          <Divider />
+      jsx: (
+        <div className={ this.props.classes.loading }>
+          <CircularProgress color='secondary' />
         </div>
-      ) : null
-    }
+      )
+    };
   }
+  // constructor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-  login(status = false) {
-    if (status) {
-      this.setState({ auth: <Button variant='contained' color='secondary' id='btn-logout' className={ this.props.classes.auth } onClick={ () => { new Voice('Sign out').activate(); setTimeout(() => window.location.reload(), 3000); } }>Sign Out</Button> });
-    } else {
-      this.setState({ auth: <Button variant='contained' color='secondary' id='btn-login' className={ this.props.classes.auth } onClick={ () => { new Voice('Sign in').activate(); } }>Sign In</Button> });
-    }
-  }
-
-  handleCompose(bool) {
-    this.setState({
-      compose: bool
-    });
-  }
-
-  handleSettings(bool) {
-    this.setState({
-      settings: bool
-    });
-  }
-
-  getEmail(profile) {
-    this.address = profile.emailAddress;
-  }
-
-  send(recipient, body, recording) {
-    // if (!!recording) {
-    //   recording.then((result) => {
-    //     this.emailContent = `MIME-Version: 1.0\r\nFrom: ${ this.address }\r\nTo: ${ recipient }\r\nSubject: Email from ${ this.address }\r\nContent-Type: multipart/related\r\nReply-To: ${ this.address }\r\nboundary="myboundary"\r\n--myboundary\r\nContent-Type: text/plain\r\n\r\n${ body }\nSent with AbleMail.\r\n\r\n--myboundary\r\nContent-Type: audio/webp\r\nContent-Transfer-Encoding: base64\r\nContent-Disposition: attachment; filename="audio.webp"\r\n\r\n${ result }`;
-    //     this.gmailSend();
-    //   });
-    // } else {
-      this.emailContent = `Content-Type: text/plain\r\nFrom: ${ this.address }\r\nTo: ${ recipient }\r\nSubject: Email from ${ this.address }\r\nReply-To: ${ this.address }\r\n\r\n${ body }\n\nSent with AbleMail.`;
-      this.gmailSend();
-    // }
-  }
-
-  gmailSend() {
-    window.gapi.client.gmail.users.messages.send({
-      userId: 'me',
-      resource: {
-        raw: btoa(this.emailContent).replace(/\+/g, '-').replace(/\//g, '_')
-      }
-    }).then(() => setTimeout(() => window.location.reload(), 500));
-  }
-
-  handleRecognition() {
-    if (this.isRecording) {
-      this.recognition.stop();
-    } else {
-      setTimeout(() => {
-        this.recognition.start(() => {
-          let message = this.recognition.transcript.toLowerCase();
-          switch (true) {
-            case /compose/.test(message):
-              document.getElementById('btn-compose').click();
-              this.recognition.transcript = '';
-              this.recognition.stop();
-              break;
-            case /read emails/.test(message):
-              new Voice(`You have ${ this.emailCount } emails. ${ this.mailData } That's all of your emails.`).activate();
-              this.recognition.transcript = '';
-              this.recognition.stop();
-              break;
-          }
+  /** componentDidMount */
+  componentDidMount() {
+    try {
+      document.addEventListener('DOMContentLoaded', () => {
+        window.gapi.load('client:auth2', () => {
+          this.gapiInit();
         });
-      }, 2000);
+      }, false);
+    } catch (e) {
+      console.error(e);
+      alert('Something went wrong. Try refreshing the page?');
     }
-    this.isRecording = !this.isRecording;
   }
+  // componentDidMount ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={ classes.body }>
-        <CssBaseline />
-        <AppBar className={ classes.appBar }>
-          <Toolbar>
-            <Settings open={ this.state.settings } onClose={ () => this.handleSettings(false) } />
-            <Button variant='outlined' color='secondary' className={ classes.settings } onClick={ () => { this.handleSettings(true); new Voice('Settings').activate() } }>
-              <SettingsIcon />&emsp;Coach's Settings
-            </Button>
-            <img onClick={ () => new Voice('AbleMail').activate() } className={ classes.logo } src='logo.png' alt='AbleMail' />
-            { this.state.auth }
-          </Toolbar>
-        </AppBar>
-        <Drawer variant='permanent' classes={ { paper: classes.drawerPaper } } className={ classes.drawer }>
-          <List component='nav'>
-            <div className={ classes.toolbar } />
-            <div className={ classes.toolbar } />
-            <Divider />
-            <ListItem button={ true } onClick={ () => { this.recognition.stop(); this.handleCompose(true); new Voice('Compose email').activate(); } } id='btn-compose'>
-              <ListItemIcon>
-                <AddCircleOutlineIcon className={ classes.icon } />
-              </ListItemIcon>
-              <ListItemText>
-                <br />
-                <Typography variant='h3' color='inherit' className={ classes.bold }>Compose</Typography>
-                <br />
-              </ListItemText>
-            </ListItem>
-            <Divider />
-            { this.state.recognition }
-            <div className={ classes.toolbar } />
-            <Divider />
-            <ListItem button={ true } onClick={ () => { this.setState({ clientSettings: true }); new Voice('Settings').activate(); } }>
-              <ListItemIcon>
-                <SettingsIcon className={ classes.icon } />
-              </ListItemIcon>
-              <ListItemText>
-                <br />
-                <ClientSettings open={ this.state.clientSettings } onClose={ () => this.setState({ clientSettings: false }) } />
-                <Typography variant='h3' color='inherit' className={ classes.bold }>Settings</Typography>
-                <br />
-              </ListItemText>
-            </ListItem>
-            <Divider />
-          </List>
-        </Drawer>
-        <main className={ classes.content }>
-          <div className={ classes.toolbar }>
-            <Mailbox isLogin={ (status) => this.login(status) } mailData={ (data) => { this.mailData = data; this.emailCount++; } } getEmail={ (profile) => this.getEmail(profile) } />
-            <Compose open={ this.state.compose } onClose={ () => this.handleCompose(false) } send={ (recipient, body, recording) => this.send(recipient, body, recording) } func={ (bool) => this.setState({ compose: bool }) } />
+  /** gapiInit
+   * @description Initializes Gmail API
+   */
+  gapiInit() {
+    window.gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(() => {
+      if (window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
+        // this.gapiGetMessages();
+        this.setState({
+          jsx: (
+            <div>
+              <TopNav status={ true } />
+              <SideNav />
+              <Mailbox />
+            </div>
+          )
+        });
+      } else {
+        this.setState({
+          jsx: (
+            <div>
+              <TopNav status={ false } />
+            </div>
+          )
+        });
+      }
+      // this.props.isLogin(window.gapi.auth2.getAuthInstance().isSignedIn.get());
+      // window.gapi.client.gmail.users.getProfile({ userId: 'me' }).then((response) => {
+      //   this.props.getEmail(JSON.parse(response.body));
+      // });
+      // try { document.getElementById('btn-login').onclick = Mailbox.login; } catch (e) {}
+      // try { document.getElementById('btn-logout').onclick = Mailbox.logout; } catch (e) {}
+    });
+  }
+  // gapiInit ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+  /** gapiGetMessages
+   * @description Fetches all emails from inbox of current account
+   */
+  gapiGetMessages() {
+    window.gapi.client.gmail.users.messages.list({
+      userId: 'me',
+      labelIds: ['INBOX']
+    }).then((response) => {
+      let { messages } = response.result;
+      this.setState({
+        jsx: (
+          <div>
+            <TopNav status='Sign Out' />
+            <SideNav />
+            <Mailbox />
           </div>
-        </main>
+        )
+      });
+    });
+  }
+  // gapiGetMessages ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+  /** render */
+  render() {
+    return (
+      <div>
+        <CssBaseline />
+        { this.state.jsx }
       </div>
     );
   }
+  // render ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 }
+// App Component ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ App Component//
 
+// Export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Export //
 export default withStyles(style)(App);
+// Export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Export //
--- src/Mailbox.js
+++ src/Mailbox.js
@@ -1,135 +1,33 @@
+// Imports ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Imports //
+// React ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import MailPreview from './MailPreview';
-import RefreshIcon from '@material-ui/icons/Refresh';
-import Typography from '@material-ui/core/Typography';
-import { Voice } from './Voice';
+// React ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-const CLIENT_ID = '<CLIENT_ID>';
-const API_KEY = '<API_KEY>';
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'];
-const SCOPES = 'https://www.googleapis.com/auth/gmail.modify';
+// Material UI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+import { withStyles } from '@material-ui/core/styles';
+// Material UI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Imports ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Imports //
 
-export default class Mailbox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mail: <CircularProgress color='secondary' />
-    };
-    this.emailCount = 0;
-    this.compiled = '';
-    this.reload = setTimeout(() => this.setState({
-      mail: (
-        <div>
-          <CircularProgress color='secondary' />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <Button color='secondary' variant='contained' onClick={ () => window.location.reload() }>
-            <RefreshIcon style={ { fontSize: JSON.parse(window.localStorage.getItem('icon')) ? '60pt': '40pt' } } />&emsp;
-            Reload
-          </Button>
-        </div>
-      )
-    }), 5000);
-  }
-  componentDidMount() {
-    try {
-      document.addEventListener('DOMContentLoaded', () => {
-        window.gapi.load('client:auth2', () => {
-          this.init();
-        });
-      }, false);
-    } catch (e) {
-      console.error(e);
-      alert('Something went wrong. Try refreshing the page?');
-      clearTimeout(this.reload);
-      this.setState({ mail: <Typography variant='h2' onClick={ () => new Voice('Unable to fetch emails').activate() }>Unable to fetch emails.</Typography> });
-    }
-  }
+// Constants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants //
+const style = theme => ({
 
-  init() {
-    window.gapi.client.init({
-      apiKey: API_KEY,
-      clientId: CLIENT_ID,
-      discoveryDocs: DISCOVERY_DOCS,
-      scope: SCOPES
-    }).then(() => {
-      if (window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
-        this.getMessages();
-      }
-      this.props.isLogin(window.gapi.auth2.getAuthInstance().isSignedIn.get());
-      window.gapi.client.gmail.users.getProfile({ userId: 'me' }).then((response) => {
-        this.props.getEmail(JSON.parse(response.body));
-      });
-      try { document.getElementById('btn-login').onclick = Mailbox.login; } catch (e) {}
-      try { document.getElementById('btn-logout').onclick = Mailbox.logout; } catch (e) {}
-    });
-  }
+});
+// Constants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants //
 
-  static login() {
-    window.gapi.auth2.getAuthInstance().signIn();
-  }
-
-  static logout() {
-    window.gapi.auth2.getAuthInstance().signOut();
-  }
-
-  static trash(id) {
-    window.gapi.client.gmail.users.messages.trash({
-      userId: 'me',
-      id: id
-    }).then(() => window.location.reload());
-  }
-
-  getMessages() {
-    window.gapi.client.gmail.users.messages.list({
-      userId: 'me',
-      labelIds: ['INBOX']
-    }).then((response) => {
-      let { messages } = response.result;
-      let fullMessage = [];
-      clearTimeout(this.reload);
-      messages.forEach((m, i) => {
-        window.gapi.client.gmail.users.messages.get({
-          userId: 'me',
-          id: m.id
-        }).then((res) => {
-          fullMessage.push(res.result);
-          if (i + 1 === messages.length) {
-            if (fullMessage.length !== messages.length) {
-              window.location.reload();
-            }
-            fullMessage.sort((a, b) => b.internalDate - a.internalDate);
-            this.setState({
-              mail: fullMessage.map((el, i) =>
-                <div key={ `preview-${ i }` }>
-                  <MailPreview snippet={ el.snippet } payload={ el.payload } trash={ () => Mailbox.trash(messages[i].id) } mailData={ (data) => this.compile(data) } />
-                  <br />
-                  <br />
-                </div>
-              )
-            });
-          }
-        });
-      });
-    });
-  }
-
-  compile(data) {
-    this.emailCount++;
-    this.compiled += `Email number ${ this.emailCount }: ${ data } `;
-    this.props.mailData(this.compiled);
-  }
-
+// Mailbox Component ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mailbox Component//
+class Mailbox extends React.Component {
+  /** render */
   render() {
     return (
       <div>
-        { this.state.mail }
+
       </div>
     );
   }
+  // render ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 }
+// Mailbox Component ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mailbox Component//
+
+// Export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Export //
+export default withStyles(style)(Mailbox);
+// Export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Export //
