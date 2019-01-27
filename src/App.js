08eee566394358d99509cd7446922e7235d85c45
Tristan Peng <pengt@catlin.edu>
2019-01-26 23:39:01 -0800

    Imports gapi.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { withStyles } from '@material-ui/core/styles';
 // Material UI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 // Other ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+import Gapi from './gapi';
 import Mailbox from './Mailbox';
 import SideNav from './SideNav';
 import TopNav from './TopNav'
@@ -17,19 +18,14 @@ import TopNav from './TopNav'
 // Imports ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Imports //
 
 // Constants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants //
-const
-  API_KEY = '<API_KEY>',
-  CLIENT_ID = '<CLIENT_ID>',
-  DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
-  SCOPES = 'https://www.googleapis.com/auth/gmail.modify',
-  style = {
-    loading: {
-      height: '100vh',
-      lineHeight: '100vh',
-      textAlign: 'center',
-      width: '100vw'
-    }
-  };
+const style = {
+  loading: {
+    height: '100vh',
+    lineHeight: '100vh',
+    textAlign: 'center',
+    width: '100vw'
+  }
+};
 // Constants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants //
 
 // App Component ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ App Component//
@@ -49,31 +45,18 @@ class App extends React.Component {
 
   /** componentDidMount */
   componentDidMount() {
-    try {
-      document.addEventListener('DOMContentLoaded', () => {
-        window.gapi.load('client:auth2', () => {
-          this.gapiInit();
+    let gapi = new Gapi();
+    gapi.connect();
+    gapi.getData.then((data) => {
+      if (!data.status) {
+        this.setState({
+          jsx: (
+            <div>
+              <TopNav status={ false } />
+            </div>
+          )
         });
-      }, false);
-    } catch (e) {
-      console.error(e);
-      alert('Something went wrong. Try refreshing the page?');
-    }
-  }
-  // componentDidMount ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
-  /** gapiInit
-   * @description Initializes Gmail API
-   */
-  gapiInit() {
-    window.gapi.client.init({
-      apiKey: API_KEY,
-      clientId: CLIENT_ID,
-      discoveryDocs: DISCOVERY_DOCS,
-      scope: SCOPES
-    }).then(() => {
-      if (window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
-        // this.gapiGetMessages();
+      } else {
         this.setState({
           jsx: (
             <div>
@@ -83,47 +66,10 @@ class App extends React.Component {
             </div>
           )
         });
-      } else {
-        this.setState({
-          jsx: (
-            <div>
-              <TopNav status={ false } />
-            </div>
-          )
-        });
       }
-      // this.props.isLogin(window.gapi.auth2.getAuthInstance().isSignedIn.get());
-      // window.gapi.client.gmail.users.getProfile({ userId: 'me' }).then((response) => {
-      //   this.props.getEmail(JSON.parse(response.body));
-      // });
-      // try { document.getElementById('btn-login').onclick = Mailbox.login; } catch (e) {}
-      // try { document.getElementById('btn-logout').onclick = Mailbox.logout; } catch (e) {}
     });
   }
-  // gapiInit ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
-
-  /** gapiGetMessages
-   * @description Fetches all emails from inbox of current account
-   */
-  gapiGetMessages() {
-    window.gapi.client.gmail.users.messages.list({
-      userId: 'me',
-      labelIds: ['INBOX']
-    }).then((response) => {
-      let { messages } = response.result;
-      this.setState({
-        jsx: (
-          <div>
-            <TopNav status='Sign Out' />
-            <SideNav />
-            <Mailbox />
-          </div>
-        )
-      });
-    });
-  }
-  // gapiGetMessages ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+  // componentDidMount ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
   /** render */
   render() {
