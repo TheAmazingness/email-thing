bd905dbc649088c30876816a2100ff4b38f434bb
Tristan Peng <pengt@catlin.edu>
2019-01-30 16:20:46 -0800

    Now displays all types of email

--- src/Mail.js
+++ src/Mail.js
@@ -48,16 +48,38 @@ const
 
 // Mail Component ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mail Component//
 class Mail extends React.Component {
+  /** static enter
+   * @desc gets HTML or plain text part of an email
+   * @param data
+   * @returns { string }
+   */
+  static enter(data) {
+    if (typeof data === 'object') {
+      for (let i = 0; i <= data.length; i++) {
+        if (typeof data[i].parts === 'undefined') {
+          if (data[i].mimeType === 'text/html') {
+            return atob(data[i].body.data.replace(/_/g, '/').replace(/-/g, '+'));
+          }
+        } else {
+          return this.enter(data[i].parts);
+        }
+      }
+      return null;
+    } else if (typeof data === 'string') {
+      return atob(data.replace(/_/g, '/').replace(/-/g, '+'));
+    }
+  }
+  // static enter ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
   /** render */
   render() {
     const { classes } = this.props;
-    console.log(this.props.data);
     return (
       <Dialog
         className={ classes.dialog }
         fullScreen
         onClose={ () => this.props.close() }
-        onEntered={ () => document.getElementById('mail').innerHTML = atob(this.props.data.replace(/_/g, '/').replace(/-/g, '+')) }
+        onEntered={ () => document.getElementById(typeof this.props.data === 'string' ? 'pre-mail' : 'div-mail').innerHTML = Mail.enter(this.props.data) }
         open={ this.props.open }
       >
         <div className={ classes.mailBody }>
@@ -75,9 +97,8 @@ class Mail extends React.Component {
           <br />
           <Divider />
           <br />
-          <pre className={ classes.pre } id='mail'>
-
-          </pre>
+          <pre className={ classes.pre } id='pre-mail' />
+          <div id='div-mail' />
         </div>
       </Dialog>
     );
