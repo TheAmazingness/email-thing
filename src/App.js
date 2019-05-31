faa79d8584617e229568b29578f058fad5c192ee
Tristan Peng <pengt@catlin.edu>
2019-05-30 20:52:02 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,78 +1,18 @@
-// Imports ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Imports //
-// React ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-import React from 'react';
-// React ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
-// Material UI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-import CircularProgress from '@material-ui/core/CircularProgress/CircularProgress';
+import React, { useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { withStyles } from '@material-ui/core/styles';
-// Material UI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+import CircularProgress from '@material-ui/core/CircularProgress';
+import style from './style';
+import { withStyles } from '@material-ui/styles';
 
-// Other ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-import Gapi from './gapi/gapi';
-import Mailbox from './Mailbox';
-import SideNav from './nav/SideNav';
-import TopNav from './nav/TopNav'
-import TTS from './features/tts';
-import { style } from './style';
-// Other ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-// Imports ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Imports //
-
-// App Component ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ App Component//
-class App extends React.Component {
-  /** constructor */
-  constructor(props) {
-    super(props);
-    this.state = {
-      jsx: (
-        <div className={ this.props.classes.loading } onClick={ () => new TTS('Loading').speak() }>
-          <CircularProgress color='secondary' />
-        </div>
-      )
-    };
-  }
-  // constructor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
-  /** componentDidMount */
-  componentDidMount() {
-    let gapi = new Gapi();
-    gapi.connect();
-    gapi.getData.then(data => {
-      if (!data.status) {
-        this.setState({ jsx: <TopNav authenticate={ () => gapi.authenticate() } status={ false } /> });
-      } else {
-        this.setState({
-          jsx: (
-            <div>
-              <TopNav authenticate={ () => gapi.authenticate() } status={ true } />
-              <SideNav />
-              <main className={ this.props.classes.content }>
-                <div className={ this.props.classes.toolbar }>
-                  <Mailbox messages={ data.messages } />
-                </div>
-              </main>
-            </div>
-          )
-        });
-      }
-    });
-  }
-  // componentDidMount ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-
-  /** render */
-  render() {
-    return (
-      <div>
-        <CssBaseline />
-        { this.state.jsx }
-      </div>
-    );
-  }
-  // render ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+function App(props) {
+  const { classes } = props;
+  const [loaded, setLoaded] = useState(<CircularProgress color="secondary" />);
+  return (
+    <div className={ classes.app }>
+      <CssBaseline/>
+      { loaded }
+    </div>
+  );
 }
-// App Component ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ App Component//
 
-// Export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Export //
 export default withStyles(style)(App);
-// Export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Export //
