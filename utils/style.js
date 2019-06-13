dbbf3ddc03f2b6fe851127fbc435dc7aad7267b2
Tristan Peng <pengt@catlin.edu>
2019-06-12 17:07:44 -0700

    JSS to CSS converter

--- utils/style.js
+++ utils/style.js
@@ -1,13 +1,17 @@
-const style = `
-  .app {
-    height: 100vh;
-    left: 0;
-    margin: 0;
-    overflow: hidden;
-    position: absolute;
-    top: 0;
-    width: 100vw;
+import json from './style.json';
+
+let style = '';
+
+for (const element in json) {
+  if (json.hasOwnProperty(element)) {
+    style += `${ element } {`;
+    for (const property in json[element]) {
+      if (json[element].hasOwnProperty(property)) {
+        style += `\n\t${ property }: ${ json[element][property] };`;
+      }
+    }
+    style += '\n}\n';
   }
-`;
+}
 
 export default style;
