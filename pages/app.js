22582b88326d652b049433e68efd01fe12d7b905
Tristan Peng <pengt@catlin.edu>
2019-06-12 17:09:10 -0700

    Testing still, but add document title and CSS Baseline

--- pages/app.js
+++ pages/app.js
@@ -1,15 +1,33 @@
+import { useState } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import TopNav from '../components/TopNav';
 import SideNav from '../components/SideNav';
 import Main from '../components/Main';
 import style from '../utils/style';
 
 const App = () => {
-  document.title = 'AbleMail';
-  return (
-    <div className="app">
+  const [load, setLoad] = useState(
+    //<div className='load-wrap'>
+    //  <CircularProgress className="load-app"/>
+    //</div>
+    <>
       <TopNav/>
       <SideNav/>
       <Main/>
+    </>
+  );
+  // setLoad(
+  //   <>
+  //     <TopNav/>
+  //     <SideNav/>
+  //     <Main/>
+  //   </>
+  // );
+  return (
+    <div className="app">
+      <CssBaseline/>
+      <title>AbleMail</title>
+      { load }
       <style global="true">{ style }</style>
     </div>
   );
