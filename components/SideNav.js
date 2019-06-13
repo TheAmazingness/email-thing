6a9a34d8f8b8d7cab0b8ca6137da0ff45644443d
Tristan Peng <pengt@catlin.edu>
2019-06-13 12:20:40 -0700

    Add navigation links

--- components/SideNav.js
+++ components/SideNav.js
@@ -1,8 +1,35 @@
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Mail as MailIcon, NoteAdd as NoteAddIcon, Settings as SettingsIcon } from '@material-ui/icons';
+
 const SideNav = () => {
   return (
-    <div>
-      2
-    </div>
+    <Drawer className="drawer" variant="permanent">
+      <div className="drawer-offset"/>
+      <List>
+        { [
+            ['Inbox', <MailIcon className="icon" />],
+            ['Send email', <NoteAddIcon className="icon" />]
+          ].map(info => (
+            <ListItem button key={ info[0] }>
+              <ListItemIcon>{ info[1] }</ListItemIcon>
+              <ListItemText className="sidenav-text" primary={ info[0] } />
+            </ListItem>
+          ))
+        }
+      </List>
+      <Divider />
+      <List>
+        <ListItem button>
+          <ListItemIcon><SettingsIcon className="icon" /></ListItemIcon>
+          <ListItemText className="sidenav-text" primary="Settings" />
+        </ListItem>
+      </List>
+    </Drawer>
   );
 };
 
