bb693bb9a272a6630cc9eb1a904937da74093150
Tristan Peng <pengt@catlin.edu>
2019-06-24 15:10:32 -0700

    Test API more

--- server.js
+++ server.js
@@ -15,7 +15,32 @@ const imap = new Imap(login);
 
 imap
   .once('ready', () => {
-    console.log(1);
+    imap.openBox('INBOX', true, (err, box) => {
+      if (err) throw err;
+      imap.seq.fetch(`1:${ box.messages.total }`, {
+        bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)', 'TEXT']
+      }).on('message', (msg, seqno) => {
+        let prefix = `(#${ seqno })`;
+        console.log(`Message #${ seqno }`);
+        msg.on('body', function(stream, info) {
+          if (info.which === 'TEXT')
+            console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
+          var buffer = '', count = 0;
+          stream.on('data', function(chunk) {
+            count += chunk.length;
+            buffer += chunk.toString('utf8');
+            if (info.which === 'TEXT')
+              console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
+          });
+          stream.once('end', function() {
+            if (info.which !== 'TEXT')
+              console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+            else
+              console.log(prefix + 'Body [%s] Finished', inspect(info.which));
+          });
+        });
+      });
+    });
   })
   .once('error', err => console.log(err))
   .once('end', () => console.log('Connection ended'))
