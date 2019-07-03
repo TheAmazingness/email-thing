b76c03ad9abdd3fe8c8a12568f3082c631c38632
Tristan Peng <pengt@catlin.edu>
2019-07-03 14:32:28 -0700

    Move ws code to Main

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,23 @@
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { useEffect } from 'react';
+
+
 const Main = () => {
+  let [mail, setMail] = useState(<CircularProgress />);
+  useEffect(() => {
+    let ws = new WebSocket('ws://localhost:8081');
+    ws.onopen = () => console.log('Connection open ...');
+    ws.onerror = err => console.error(err);
+    ws.onclose = () => console.log('Connection closed...');
+    ws.onmessage = event => {
+      let data = JSON.parse(event.data);
+      console.log(data);
+    };
+  });
+
   return (
     <main>
-      3
+      { mail }
     </main>
   );
 };
