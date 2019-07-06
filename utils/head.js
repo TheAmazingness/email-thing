060c177fa1aa6f96c4ba6ab0e63424126bb7c5f6
Tristan Peng <pengt@catlin.edu>
2019-07-05 20:08:57 -0700

    Use CSS instead of a JSON file for CSS

--- utils/head.js
+++ utils/head.js
@@ -1,12 +1,11 @@
 import Head from 'next/head';
-import style from './style';
 
 const CustomHead = () => {
   return (
     <Head>
       <title>AbleMail</title>
       <link rel="shortcut icon" href="../static/favicon.ico" />
-      <style global="true">{ style }</style>
+      <link rel="stylesheet" href="../static/style.css" />
     </Head>
   );
 };
