90b3dbcf2a0539a5011886817506ca75fa9ae9ba
Tristan Peng <pengt@catlin.edu>
2019-07-21 16:59:37 -0700

    Style settings page and faster redirect if not logged in

--- pages/settings.js
+++ pages/settings.js
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Link from 'next/link';
@@ -14,16 +15,67 @@ import { Check as CheckIcon, Settings as SettingsIcon } from '@material-ui/icons
 const Settings = () => {
   let setting;
   let checked;
+  const [body, setBody] = useState(
+    <div className="load-wrap">
+      <CircularProgress className="load-app" />
+    </div>
+  );
   const [settings, setSettings] = useState({
     tts: false
   });
   useEffect(() => {
     const credentials = JSON.parse(localStorage.getItem('login'));
-    !credentials ? location.href = `${ location.href.split('/')[0] }/app` : null;
-    const get = item => !!localStorage.getItem(item) ? JSON.parse(localStorage.getItem(item)) : false;
-    setSettings({
-      tts: get('tts')
-    });
+    if (!credentials) {
+      location.href = `${ location.href.split('/')[0] }/app`;
+    } else {
+      setBody(
+        <>
+          <CssBaseline />
+          <CustomHead />
+          <TopNav onLogout={ () => {
+            localStorage.removeItem('login');
+            location.href = `${ location.href.split('/')[0] }/app`;
+          } } />
+          <div className="settings-offset" />
+          <Grid className="settings-body" container spacing={ 8 }>
+            <Grid className="settings-title" item sm={ 12 }>
+              <SettingsIcon className="settings-icon" />
+              &emsp;Settings
+            </Grid>
+            <Grid item sm={ 4 }>
+              <Card>
+                <CardContent className="settings-name">
+                  Text-to-Speech
+                </CardContent>
+                <CardActions className="settings-action">
+                  <Switch
+                    checked={ settings.tts }
+                    onChange={ () => {
+                      setting = 'tts';
+                      checked = !settings.tts;
+                      setSettings({ ...settings, 'tts': checked });
+                    } }
+
+                  />
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid className="settings-apply" item sm={ 12 }>
+              <Link href="/app">
+                <Button color="primary" size="large" variant="outlined">
+                  <CheckIcon />
+                  &emsp;Apply
+                </Button>
+              </Link>
+            </Grid>
+          </Grid>
+        </>
+      );
+      const get = item => !!localStorage.getItem(item) ? JSON.parse(localStorage.getItem(item)) : false;
+      setSettings({
+        tts: get('tts')
+      });
+    }
   }, []);
   useEffect(() => {
     localStorage.setItem(setting, checked);
@@ -31,39 +83,7 @@ const Settings = () => {
   return (
     <>
       <CssBaseline />
-      <CustomHead />
-      <TopNav onLogout={ () => {
-        localStorage.removeItem('login');
-        location.href = `${ location.href.split('/')[0] }/app`;
-      } } />
-      <div className="settings-offset" />
-      <SettingsIcon />
-      &emsp;Settings
-      <Grid container spacing={ 8 }>
-        <Grid item sm={ 6 }>
-          <Card>
-            <CardContent>
-              Text-to-Speech
-            </CardContent>
-            <CardActions>
-              <Switch
-                checked={ settings.tts }
-                onChange={ () => {
-                  setting = 'tts';
-                  checked = !settings.tts;
-                  setSettings({ ...settings, 'tts': checked });
-                } }
-              />
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
-      <Link href="/app">
-        <Button color="primary">
-          <CheckIcon />
-          &emsp;Apply
-        </Button>
-      </Link>
+      { body }
     </>
   );
 };
