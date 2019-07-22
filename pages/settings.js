73f4e134e494dd5d5024fb3d50a9c3137e5f7bf9
Tristan Peng <pengt@catlin.edu>
2019-07-22 14:51:44 -0700

    Abstract setting and fix bug where switch would not update state

--- pages/settings.js
+++ pages/settings.js
@@ -1,28 +1,20 @@
 import { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Link from 'next/link';
-import Switch from '@material-ui/core/Switch';
 import CustomHead from '../components/Head';
+import Setting from '../components/Setting';
 import TopNav from '../components/TopNav';
 import { Check as CheckIcon, Settings as SettingsIcon } from '@material-ui/icons';
 
 const Settings = () => {
-  let setting;
-  let checked;
   const [body, setBody] = useState(
     <div className="load-wrap">
       <CircularProgress className="load-app" />
     </div>
   );
-  const [settings, setSettings] = useState({
-    tts: false
-  });
   useEffect(() => {
     const credentials = JSON.parse(localStorage.getItem('login'));
     if (!credentials) {
@@ -43,22 +35,7 @@ const Settings = () => {
               &emsp;Settings
             </Grid>
             <Grid item sm={ 4 }>
-              <Card>
-                <CardContent className="settings-name">
-                  Text-to-Speech
-                </CardContent>
-                <CardActions className="settings-action">
-                  <Switch
-                    checked={ settings.tts }
-                    onChange={ () => {
-                      setting = 'tts';
-                      checked = !settings.tts;
-                      setSettings({ ...settings, 'tts': checked });
-                    } }
-
-                  />
-                </CardActions>
-              </Card>
+              <Setting name="tts">Text-to-Speech</Setting>
             </Grid>
             <Grid className="settings-apply" item sm={ 12 }>
               <Link href="/app">
@@ -71,15 +48,8 @@ const Settings = () => {
           </Grid>
         </>
       );
-      const get = item => !!localStorage.getItem(item) ? JSON.parse(localStorage.getItem(item)) : false;
-      setSettings({
-        tts: get('tts')
-      });
     }
   }, []);
-  useEffect(() => {
-    localStorage.setItem(setting, checked);
-  }, [settings]);
   return (
     <>
       <CssBaseline />
