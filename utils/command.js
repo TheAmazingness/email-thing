1ba622c825b40e2d663c5d6dd3e940b2a6fd8132
Tristan Peng <pengt@catlin.edu>
2019-08-26 11:41:41 -0700

    Make more compact and fix bug where disabling voice commands threw an error

--- utils/command.js
+++ utils/command.js
@@ -1,16 +1,14 @@
 const command = () => {
-  if (JSON.parse(localStorage.getItem('recognition'))) {
-    return new Promise(resolve => {
-      // let sr = new (SpeechRecognition || webkitSpeechRecognition || mozSpeechRecognition || msSpeechRecognition)();
-      let sr = new webkitSpeechRecognition();
-      sr.lang = 'en-US';
-      sr.start();
-      sr.onresult = e => {
-        sr.stop();
-        resolve(e.results[0][0].transcript);
-      };
-    });
-  }
+  return JSON.parse(localStorage.getItem('recognition')) ? new Promise(resolve => {
+    // let sr = new (SpeechRecognition || webkitSpeechRecognition || mozSpeechRecognition || msSpeechRecognition)();
+    let sr = new webkitSpeechRecognition();
+    sr.lang = 'en-US';
+    sr.start();
+    sr.onresult = e => {
+      sr.stop();
+      resolve(e.results[0][0].transcript);
+    };
+  }) : '';
 };
 
 export default command;
