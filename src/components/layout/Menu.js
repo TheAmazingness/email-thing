8f042d8a6b062ba938c859192ad3d6bbc2533eba
Tristan Peng <pengt@catlin.edu>
2020-02-13 08:56:32 -0800

    Finish inbox

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -4,11 +4,39 @@ import { Link } from 'react-router-dom';
 const Menu = () => (
   <aside className="menu">
     <p className="menu-label">
+      <i className="fas fa-inbox" />
+      &nbsp;
       Mail
     </p>
     <ul className="menu-list">
-      <li><Link to="/inbox">Inbox</Link></li>
-      <li><Link to="/compose">Compose</Link></li>
+      <li>
+        <Link to="/inbox">
+          <i className="fas fa-inbox" />
+          &nbsp;
+          Inbox
+        </Link>
+      </li>
+      <li>
+        <Link to="/compose">
+          <i className="fas fa-edit" />
+          &nbsp;
+          Compose
+        </Link>
+      </li>
+    </ul>
+    <p className="menu-label">
+      <i className="fas fa-cogs" />
+      &nbsp;
+      Settings
+    </p>
+    <ul className="menu-list">
+      <li>
+        <Link to="/settings">
+          <i className="fas fa-cog" />
+          &nbsp;
+          Settings
+        </Link>
+      </li>
     </ul>
   </aside>
 );
--- src/components/mail/Inbox.js
+++ src/components/mail/Inbox.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MailItem from './MailItem';
 
 const Inbox = ({ mail }) => {
-  const list = mail ? mail.map(m => (
-    <MailItem mail={ m } key={ m.id } />
-  )) : (
-    <div className="container no-mail">
-      <div className="level no-mail">
-        <div className="level-item has-text-centered">
-          <h1 className="title">
-            <i className="fas fa-folder-open" />
-            &nbsp;
-            No mail!
-          </h1>
+  const [state, setState] = useState({
+    noMail: (
+      <div className="container no-mail">
+        <div className="level no-mail">
+          <div className="level-item has-text-centered">
+            <progress className="progress is-primary" max="100" />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  });
+  const list = mail ? mail.map(m => <MailItem mail={ m } key={ m.id } />) : state.noMail;
+
+  if (!mail) {
+    setTimeout(() => setState({
+      ...state,
+      noMail: (
+        <div className="container no-mail">
+          <div className="level no-mail">
+            <div className="level-item has-text-centered">
+              <h1 className="title is-1 has-text-primary">
+                <i className="fas fa-folder-open" />
+                &nbsp;
+                No mail!
+              </h1>
+            </div>
+          </div>
+        </div>
+      )
+    }), 5000);
+  }
+
   return (
     <div className="inbox">
       { list }
